bb52cf76dfcc6ec4a8f673ce6c959d8ce9a34b72
skuroedov <me@sergeykuroedov.cz>
2021-06-13 22:50:11 +0200

    Added CSS

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
+use gtk::gdk::Display;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, CssProvider, StyleContext};
 use visivi::MainWindow::MainWindow;
 
 fn main() {
     let app = Application::new(Some("me.sergeykuroedov.visivi"), Default::default());
-    app.connect_activate(build_ui);
+    app.connect_activate(|app| {
+        setup_css();
+        build_ui(app);
+    });
     app.run();
 }
 
 fn build_ui(application: &Application) {
     let window = MainWindow::new(application)
         .present();
+}
+
+fn setup_css() {
+    let provider = CssProvider::new();
+    provider.load_from_data(include_bytes!("res/style.css"));
+    StyleContext::add_provider_for_display(
+        &Display::default().expect("Error initializing gtk css provider."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
 }
